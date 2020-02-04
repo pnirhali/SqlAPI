1b721c0171f7371fd85866850845becaa2c1523b
Poonam <Poonamdhimate@gmail.com>
2020-02-04 11:07:10 +1100

    Added a test for the OperationResolver class

---
+++ SqlAPITests/Services/OperationResolverTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SqlAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlAPI.Services.Tests
+{
+    [TestClass]
+    public class OperationResolverTests
+    {
+        [TestMethod]
+        public void ResolveOperation_For_NullOperation_Throws_ArgumentNullException()
+        {
+            // Mock the dependencies
+            var loggerMock = new Mock<ILogger<OperationResolver>>();
+            var serviceProviderMock = new Mock<IServiceProvider>();
+
+            // Call and assert
+            var operationResolver = new OperationResolver(serviceProviderMock.Object, loggerMock.Object);
+            Assert.ThrowsException<ArgumentNullException>(() => operationResolver.ResolveOperation(null));
+        }
+    }
+}
