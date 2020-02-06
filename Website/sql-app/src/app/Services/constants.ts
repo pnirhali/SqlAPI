15262d58685fd3686ce34a89f08083dc838c6757
Poonam <Poonamdhimate@gmail.com>
2020-02-07 09:42:16 +1100

    MInor typo

--- Website/sql-app/src/app/Services/constants.ts
+++ Website/sql-app/src/app/Services/constants.ts
@@ -1,4 +1,4 @@
 // APIEndpoints 
 export const BaseUrl = 'https://localhost:44385/api';
 export const GenerateQueryEndpoint = BaseUrl + '/Query/Generate';
-export const ExecuteQueryEndpoint = BaseUrl + 'Query/Execute';
+export const ExecuteQueryEndpoint = BaseUrl + '/Query/Execute';
